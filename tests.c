f864f41dea460c6755c3e7acc768710b40f21600
Mike Levenick <michael.levenick@gmail.com>
2024-05-01 23:39:56 -0500

    Added tests and reformat

--- tests.c
+++ tests.c
@@ -19,11 +19,12 @@ static char * test_create_double_matrix() {
     // When
     // Create a 2x2 matrix
     Matrix mat = createMatrix(2, 2, DOUBLE);
+
+    // Then
     printf("Created matrix:\n");
     printMatrix(mat);
 
-    // Then
-    // Make sure it has 2 rows and 2 columns. If it doesn't, return the message
+    // Make sure it has 2 rows and 2 columns
     mu_assert("TEST FAILED: mat.rows != 2", mat.rows == 2);
     mu_assert("TEST FAILED: mat.cols != 2", mat.cols == 2);
 
@@ -46,11 +47,12 @@ static char * test_create_int_matrix() {
     // When
     // Create a 2x2 matrix
     Matrix mat = createMatrix(2, 2, INT);
+
+    // Then
     printf("Created matrix:\n");
     printMatrix(mat);
 
-    // Then
-    // Make sure it has 2 rows and 2 columns. If it doesn't, return the message
+    // Make sure it has 2 rows and 2 columns
     mu_assert("TEST FAILED: mat.rows != 2", mat.rows == 2);
     mu_assert("TEST FAILED: mat.cols != 2", mat.cols == 2);
 
@@ -73,11 +75,12 @@ static char * test_create_char_matrix() {
     // When
     // Create a 2x2 matrix
     Matrix mat = createMatrix(2, 2, CHAR);
+
+    // Then
     printf("Created matrix:\n");
     printMatrix(mat);
 
-    // Then
-    // Make sure it has 2 rows and 2 columns. If it doesn't, return the message
+    // Make sure it has 2 rows and 2 columns
     mu_assert("TEST FAILED: mat.rows != 2", mat.rows == 2);
     mu_assert("TEST FAILED: mat.cols != 2", mat.cols == 2);
 
@@ -107,10 +110,11 @@ static char * test_matrix_operations() {
     // When
     // Set the item at 0,0 to our element of value 5
     setMatrixElement(&mat, 0, 0, element);
+
+    // Then
     printf("Matrix after setting element at 0,0:\n");
     printMatrix(mat);
 
-    // Then
     // Ensure the element at 0,0 is valued 5 as expected, or return a message
     mu_assert("TEST FAILED: mat.data[0][0].int_val != 5", mat.data[0][0].int_val == 5);
 
@@ -142,10 +146,10 @@ static char * test_set_row() {
     // Set the first row with an array of values
     setRowOrColumn(&mat, 0, ROW, vals, 3);
 
+    // Then
     printf("Matrix after setting row 0 with values of 5:\n");
     printMatrix(mat);
 
-    // Then
     // Check if all elements in the first row are set correctly
     for (int i = 0; i < mat.cols; i++) {
         mu_assert("Error: Element not set correctly in row", mat.data[0][i].int_val == 5);
@@ -177,10 +181,10 @@ static char* test_set_column() {
     // Set the second column with an array of values
     setRowOrColumn(&mat, 1, COL, vals, 3);
 
+    // Then
     printf("Matrix after setting Column:\n");
     printMatrix(mat);
 
-    // Then
     // Check if all elements in the second column are set correctly
     for (int i = 0; i < mat.rows; i++) {
         mu_assert("Error: Element not set correctly in column", mat.data[i][1].int_val == 10);
@@ -216,10 +220,11 @@ static char * test_create_matrix_subset_complete() {
     // When
     // Attempt to create a smaller matrix as a subset of the original
     Matrix subset = createMatrixSubset(original, 0, 2, 0, 2);
+    
+    // Then
     printf("Subset matrix:\n");
     printMatrix(subset);
 
-    // Then
     // Subset should have 3 rows and 3 columns
     mu_assert("TEST FAILED: subset.rows != 3", subset.rows == 3);
     mu_assert("TEST FAILED: subset.cols != 3", subset.cols == 3);
@@ -258,12 +263,15 @@ static char * test_create_matrix_subset_single_element() {
     // Create matrix from just the middle element
     Matrix subset = createMatrixSubset(original, 2, 2, 2, 2);
 
+    // Then
     printf("Single-element subset matrix:\n");
     printMatrix(subset);
 
-    // Then
+    // Check row and column numbers
     mu_assert("TEST FAILED: subset.rows != 1", subset.rows == 1);
     mu_assert("TEST FAILED: subset.cols != 1", subset.cols == 1);
+
+    // Check subset element
     mu_assert("TEST FAILED: subset element does not match original single value", subset.data[0][0].double_val == 1.234);
 
     // Cleanup
@@ -297,12 +305,15 @@ static char * test_resize_matrix_increase() {
     // Rresize the matrix to 3x3
     resizeMatrix(&mat, 3, 3);
 
+    // Then
     printf("Matrix after resizing:\n");
     printMatrix(mat);
 
-    // Then
+    // Check row and column numbers
     mu_assert("TEST FAILED: mat.rows != 3", mat.rows == 3);
     mu_assert("TEST FAILED: mat.cols != 3", mat.cols == 3);
+    
+    // Check a few random cells
     mu_assert("TEST FAILED: mat.data[0][0].int_val != 1", mat.data[0][0].int_val == 1);
     mu_assert("TEST FAILED: mat.data[1][1].int_val != 4", mat.data[1][1].int_val == 4);
     mu_assert("TEST FAILED: uninitialized cell should be 0 for int", mat.data[2][2].int_val == 0);
@@ -333,12 +344,15 @@ static char * test_resize_matrix_decrease() {
     // Resize the matrix to 2x2
     resizeMatrix(&mat, 2, 2);
 
+    // Then
     printf("Resized matrix:\n");
     printMatrix(mat);
 
-    // Then
+    // Check row and column numbers
     mu_assert("TEST FAILED: mat.rows != 2", mat.rows == 2);
     mu_assert("TEST FAILED: mat.cols != 2", mat.cols == 2);
+
+    // Check out of bounds
     mu_assert("TEST FAILED: data beyond new dimensions should not be accessible", mat.data[1][1].int_val == 0);
 
     // Cleanup
@@ -362,16 +376,19 @@ static char * test_resize_matrix_to_zero() {
     printf("Initial matrix:\n");
     printMatrix(mat);
 
+    // When
     // Resize it down to 0x0
     resizeMatrix(&mat, 0, 0);
 
+    // Then
     printf("Resized matrix:\n");
     printMatrix(mat);
 
+    // Check row and column numbers
     mu_assert("TEST FAILED: mat.rows should be 0", mat.rows == 0);
     mu_assert("TEST FAILED: mat.cols should be 0", mat.cols == 0);
 
-    // Here, we assume mat.data should be NULL or handled gracefully
+    // Check that data should be null or invalid // TODO - MAKE THIS isValid = 0
     mu_assert("TEST FAILED: mat.data should be NULL or invalid", mat.data == NULL || mat.cols == 0);
 
     // Cleanup
@@ -468,12 +485,15 @@ static char * test_adding_integer_matrix() {
     // Add the two matricies together
     Matrix matResult = addMatrices(&mat1, &mat2);
 
+    // Then
     printf("Resulting matrix after addition:\n");
     printMatrix(matResult);
 
+    // Check row and column numbers
     mu_assert("TEST FAILED: matrix should have 4 rows", matResult.rows == 4);
     mu_assert("TEST FAILED: matrix should have 4 columns", matResult.cols == 4);
 
+    // Check a few random cells
     mu_assert("TEST FAILED: cell 0,0 should be 10", matResult.data[0][0].int_val == 10);
     mu_assert("TEST FAILED: cell 1,0 should be 18", matResult.data[1][0].int_val == 18);
     mu_assert("TEST FAILED: cell 3,3 should be 40", matResult.data[3][3].int_val == 40);
@@ -495,7 +515,7 @@ static char * test_adding_double_matrix() {
     printf("*** TEST START: %s ***\n", functionName);
 
     // Given
-    // Create a 4x4 matrix filled with ints
+    // Create a 4x4 matrix filled with doubles
     Matrix mat1 = createMatrix(4, 4, DOUBLE);
     for (int r = 0; r < 4; r++) {
         for (int c = 0; c < 4; c++) {
@@ -506,7 +526,7 @@ static char * test_adding_double_matrix() {
     printf("Initial matrix 1:\n");
     printMatrix(mat1);
 
-    // Create a 4x4 matrix filled with ints, 10 higher than the previous one.
+    // Create a 4x4 matrix filled with doubles, 10 higher than the previous one.
     Matrix mat2 = createMatrix(4, 4, DOUBLE);
     for (int r = 0; r < 4; r++) {
         for (int c = 0; c < 4; c++) {
@@ -521,12 +541,15 @@ static char * test_adding_double_matrix() {
     // Add the two matricies together
     Matrix matResult = addMatrices(&mat1, &mat2);
 
+    // Then
     printf("Resulting matrix after addition:\n");
     printMatrix(matResult);
-
+    
+    // Check row and column numbers
     mu_assert("TEST FAILED: matrix should have 4 rows", matResult.rows == 4);
     mu_assert("TEST FAILED: matrix should have 4 columns", matResult.cols == 4);
 
+    // Check a few random cells
     mu_assert("TEST FAILED: cell 0,0 should be 10", matResult.data[0][0].double_val == 10.0);
     mu_assert("TEST FAILED: cell 1,0 should be 18", matResult.data[1][0].double_val == 18.0);
     mu_assert("TEST FAILED: cell 3,3 should be 40", matResult.data[3][3].double_val == 40.0);
@@ -541,6 +564,281 @@ static char * test_adding_double_matrix() {
     return NULL;
 }
 
+// Matrix subtraction
+// Integer
+static char * test_subtracting_integer_matrix() {
+    // Intro output
+    const char *functionName = "Subtract Matrix - Integer";
+    printf("*** TEST START: %s ***\n", functionName);
+
+    // Given
+    // Create a 4x4 matrix filled with ints
+    Matrix mat1 = createMatrix(4, 4, INT);
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
+            mat1.data[r][c].int_val = r * 4 + c + 10;
+        }
+    }
+
+    printf("Initial matrix 1:\n");
+    printMatrix(mat1);
+
+    // Create a 4x4 matrix filled with ints, 10 lower than the previous one.
+    Matrix mat2 = createMatrix(4, 4, INT);
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
+            mat2.data[r][c].int_val = r * 4 + c;
+        }
+    }
+
+    printf("Initial matrix 2:\n");
+    printMatrix(mat2);
+
+    // When
+    // Subtract matrix 2 from matrix 1
+    Matrix matResult = subtractMatrices(&mat1, &mat2);
+
+    // Then
+    printf("Resulting matrix after subtraction:\n");
+    printMatrix(matResult);
+
+    // Check row and column numbers
+    mu_assert("TEST FAILED: matrix should have 4 rows", matResult.rows == 4);
+    mu_assert("TEST FAILED: matrix should have 4 columns", matResult.cols == 4);
+
+    // Check a few random cells
+    mu_assert("TEST FAILED: cell 0,0 should be 10", matResult.data[0][0].int_val == 10);
+    mu_assert("TEST FAILED: cell 1,0 should be 18", matResult.data[1][0].int_val == 10);
+    mu_assert("TEST FAILED: cell 3,3 should be 40", matResult.data[3][3].int_val == 10);
+
+    // Cleanup
+    freeMatrix(&mat1);
+    freeMatrix(&mat2);
+    freeMatrix(&matResult);
+
+    // Success output
+    printf("*** TEST PASSED: %s ***\n", functionName);
+    return NULL;
+}
+
+// Double
+static char * test_subtracting_double_matrix() {
+    // Intro output
+    const char *functionName = "Subtract Matrix - Double";
+    printf("*** TEST START: %s ***\n", functionName);
+
+    // Given
+    // Create a 4x4 matrix filled with doubles
+    Matrix mat1 = createMatrix(4, 4, DOUBLE);
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
+            mat1.data[r][c].double_val = (double)r * 4 + c + 10;
+        }
+    }
+
+    printf("Initial matrix 1:\n");
+    printMatrix(mat1);
+
+    // Create a 4x4 matrix filled with doubles, 10 lower than the previous one.
+    Matrix mat2 = createMatrix(4, 4, DOUBLE);
+    for (int r = 0; r < 4; r++) {
+        for (int c = 0; c < 4; c++) {
+            mat2.data[r][c].double_val = (double)r * 4 + c;
+        }
+    }
+
+    printf("Initial matrix 2:\n");
+    printMatrix(mat2);
+
+    // When
+    // Subtract matrix 2 from matrix 1
+    Matrix matResult = subtractMatrices(&mat1, &mat2);
+
+    // Then
+    printf("Resulting matrix after subtraction:\n");
+    printMatrix(matResult);
+
+    // Check row and column numbers
+    mu_assert("TEST FAILED: matrix should have 4 rows", matResult.rows == 4);
+    mu_assert("TEST FAILED: matrix should have 4 columns", matResult.cols == 4);
+
+    // Check a few random cells
+    mu_assert("TEST FAILED: cell 0,0 should be 10", matResult.data[0][0].double_val == 10.0);
+    mu_assert("TEST FAILED: cell 1,0 should be 10", matResult.data[1][0].double_val == 10.0);
+    mu_assert("TEST FAILED: cell 3,3 should be 10", matResult.data[3][3].double_val == 10.0);
+
+    // Cleanup
+    freeMatrix(&mat1);
+    freeMatrix(&mat2);
+    freeMatrix(&matResult);
+
+    // Success output
+    printf("*** TEST PASSED: %s ***\n", functionName);
+    return NULL;
+}
+
+// Matrix Multiplication
+// Integer
+static char * test_multiplying_integer_matrix() {
+    // Intro output
+    const char *functionName = "Multiply Matrix - Integer";
+    printf("*** TEST START: %s ***\n", functionName);
+
+    // Given
+    // Create a 2x2 matrix filled with ints
+    Matrix mat1 = createMatrix(2, 2, INT);
+    for (int r = 0; r < 2; r++) {
+        for (int c = 0; c < 2; c++) {
+            mat1.data[r][c].int_val = r * 2 + c + 1;
+        }
+    }
+
+    printf("Initial matrix 1:\n");
+    printMatrix(mat1);
+
+    // Create a 2x2 matrix filled with the same ints
+    Matrix mat2 = createMatrix(2, 2, INT);
+    for (int r = 0; r < 2; r++) {
+        for (int c = 0; c < 2; c++) {
+            mat2.data[r][c].int_val = r * 2 + c + 1;
+        }
+    }
+
+    printf("Initial matrix 2:\n");
+    printMatrix(mat2);
+
+    // When
+    // Multipy them together
+    Matrix matResult = multiplyMatrices(&mat1, &mat2);
+
+    // Then
+    printf("Resulting matrix after multiplication:\n");
+    printMatrix(matResult);
+
+    // Check row and column numbers
+    mu_assert("TEST FAILED: matrix should have 2 rows", matResult.rows == 2);
+    mu_assert("TEST FAILED: matrix should have 2 columns", matResult.cols == 2);
+
+    // Check a few random cells
+    mu_assert("TEST FAILED: cell 0,0 should be 7", matResult.data[0][0].int_val == 7);
+    mu_assert("TEST FAILED: cell 1,0 should be 15", matResult.data[1][0].int_val == 15);
+    mu_assert("TEST FAILED: cell 0,1 should be 10", matResult.data[0][1].int_val == 10);
+
+    // Cleanup
+    freeMatrix(&mat1);
+    freeMatrix(&mat2);
+    freeMatrix(&matResult);
+
+    // Success output
+    printf("*** TEST PASSED: %s ***\n", functionName);
+    return NULL;
+}
+
+// Double
+static char * test_multiplying_double_matrix() {
+    // Intro output
+    const char *functionName = "Multiply Matrix - Double";
+    printf("*** TEST START: %s ***\n", functionName);
+
+    // Given
+    // Create a 2x2 matrix filled with doubles
+    Matrix mat1 = createMatrix(2, 2, DOUBLE);
+    for (int r = 0; r < 2; r++) {
+        for (int c = 0; c < 2; c++) {
+            mat1.data[r][c].double_val = (double)r * 2 + c + 1;
+        }
+    }
+
+    printf("Initial matrix 1:\n");
+    printMatrix(mat1);
+
+    // Create a 2x2 matrix filled with the same doubles
+    Matrix mat2 = createMatrix(2, 2, DOUBLE);
+    for (int r = 0; r < 2; r++) {
+        for (int c = 0; c < 2; c++) {
+            mat2.data[r][c].double_val = (double)r * 2 + c + 1;
+        }
+    }
+
+    printf("Initial matrix 2:\n");
+    printMatrix(mat2);
+
+    // When
+    // Multipy them together
+    Matrix matResult = multiplyMatrices(&mat1, &mat2);
+
+    // Then
+    printf("Resulting matrix after multiplication:\n");
+    printMatrix(matResult);
+
+    // Check row and column numbers
+    mu_assert("TEST FAILED: matrix should have 2 rows", matResult.rows == 2);
+    mu_assert("TEST FAILED: matrix should have 2 columns", matResult.cols == 2);
+
+    // Check a few random cells
+    mu_assert("TEST FAILED: cell 0,0 should be 7", matResult.data[0][0].double_val == 7.0);
+    mu_assert("TEST FAILED: cell 1,0 should be 15", matResult.data[1][0].double_val == 15.0);
+    mu_assert("TEST FAILED: cell 0,1 should be 10", matResult.data[0][1].double_val == 10.0);
+
+    // Cleanup
+    freeMatrix(&mat1);
+    freeMatrix(&mat2);
+    freeMatrix(&matResult);
+
+    // Success output
+    printf("*** TEST PASSED: %s ***\n", functionName);
+    return NULL;
+}
+
+// Invalid Data Type
+static char * test_multiplying_invalid_data_matrix() {
+    // Intro output
+    const char *functionName = "Multiply Matrix - Invalid Data Types";
+    printf("*** TEST START: %s ***\n", functionName);
+
+    // Given
+    // Create a 2x2 matrix filled with doubles
+    Matrix mat1 = createMatrix(2, 2, DOUBLE);
+    for (int r = 0; r < 2; r++) {
+        for (int c = 0; c < 2; c++) {
+            mat1.data[r][c].double_val = (double)r * 2 + c + 1;
+        }
+    }
+
+    printf("Initial matrix 1:\n");
+    printMatrix(mat1);
+
+    // Create a 2x2 matrix filled with the same ints
+    Matrix mat2 = createMatrix(2, 2, INT);
+    for (int r = 0; r < 2; r++) {
+        for (int c = 0; c < 2; c++) {
+            mat2.data[r][c].int_val = r * 2 + c + 1;
+        }
+    }
+
+    printf("Initial matrix 2:\n");
+    printMatrix(mat2);
+
+    // When
+    // Multipy them together
+    Matrix matResult = multiplyMatrices(&mat1, &mat2);
+
+    printf("Resulting matrix after multiplication:\n");
+    printMatrix(matResult);
+
+    // Then
+    // Matrix should be invalid
+    mu_assert("TEST FAILED: matrix should be invalid", isValid(&matResult) == 0);
+
+    // Cleanup
+    freeMatrix(&mat1);
+    freeMatrix(&mat2);
+    freeMatrix(&matResult);
+
+    // Success output
+    printf("*** TEST PASSED: %s ***\n", functionName);
+    return NULL;
+}
 
 // Run the tests
 static char * all_tests() {
@@ -561,6 +859,11 @@ static char * all_tests() {
     mu_run_test(test_set_matrix_subset);
     mu_run_test(test_adding_integer_matrix);
     mu_run_test(test_adding_double_matrix);
+    mu_run_test(test_subtracting_integer_matrix);
+    mu_run_test(test_subtracting_double_matrix);
+    mu_run_test(test_multiplying_integer_matrix);
+    mu_run_test(test_multiplying_double_matrix);
+    mu_run_test(test_multiplying_invalid_data_matrix);
     return 0;
 }
 
