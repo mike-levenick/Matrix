336b453de1a2ebfccba156839568313e7dcf95a6
Mike Levenick <michael.levenick@gmail.com>
2024-05-03 20:42:19 -0500

    Update minunit.h

--- minunit.h
+++ minunit.h
@@ -5,10 +5,18 @@
 #include <string.h>
 
 // Accumulate test results
+// Total tests ran
 extern int tests_run;
-extern int tests_failed;
-extern char test_details[1024]; // Increase size as needed for detailed output
 
+// Tests failed
+extern int tests_failed;
+
+// Details about the failures
+extern char test_details[1024];
+
+// Assertion
+// If the test fails (test == false), add the test details for the test to the buffer
+// And then return "fail"
 #define mu_assert(message, test) do { \
     if (!(test)) { \
         sprintf(test_details + strlen(test_details), "FAIL: %s - %s\n", __func__, message); \
@@ -16,6 +24,8 @@ extern char test_details[1024]; // Increase size as needed for detailed output
     } \
 } while (0)
 
+// For each test that is run, increment the number of tests run
+// If we get a message back (which indicates a failure), increment failed_tests
 #define mu_run_test(test) do { \
     char *message = test(); \
     tests_run++; \
